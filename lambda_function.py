bf9d4a64433d6bed0fad329c006571a9cb8e1c32
Dario Paleit <paleit@th-brandenburg.de>
2019-01-24 18:39:34 +0100

    added better comments

--- lambda_function.py
+++ lambda_function.py
@@ -3,34 +3,34 @@ import os
 import pymysql
 
 
-# test
 def lambda_handler(event, context):
-    # on session start : greetings and cloud formation stack info
+    # on session start - output: welcome message and cloud formation stack info
     if event["session"]["new"]:
         on_session_started({"requestId": event["request"]["requestId"]}, event["session"])
     if event["request"]["type"] == "LaunchRequest":
         return on_launch(event["request"], event["session"])
-    # on intent
+    # if the event was triggered by an uttered intent: check the intent name
     elif event["request"]["type"] == "IntentRequest":
         return on_intent(event["request"], event["session"])
-    # on session end : farewell and cloud formation stack info
+    # on session end - output: farewell and cloud formation stack info
     elif event["request"]["type"] == "SessionEndedRequest":
         return on_session_ended(event["request"], event["session"])
 
 
+# check the intent name to get the intent type
 def on_intent(intent_request, session):
     intent = intent_request["intent"]
     intent_name = intent_request["intent"]["name"]
-    # our custom intents
+    # the custom intents - output: will be determined later
     if intent_name == "TestIntent":
         return get_test_value()
     elif intent_name == "ProjectSizeByLanguage":
-        return get_project_size_by_language()
+        return get_project_size_by_language(intent)
     elif intent_name == "CommitsWithCursesByLanguage":
-        return get_commits_with_curses_by_language()
+        return get_commits_with_curses_by_language(intent)
     elif intent_name == "LanguagesUsedTogether":
-        return get_languages_used_together()
-    # the amazon-made Intents : basically placeholders, triggers farewell as well
+        return get_languages_used_together(intent)
+    # the obligatory amazon-made Intents - output: basically placeholders
     elif intent_name == "AMAZON.FallBackIntent":
         return handle_filler_request()
     elif intent_name == "AMAZON.HelpIntent":
@@ -43,12 +43,12 @@ def on_intent(intent_request, session):
         raise ValueError("Invalid intent")
 
 
-# session is started -> message for our cloud formation stack
+# session is started - output: message for our cloud formation stack
 def on_session_started(session_started_request, session):
     print("Starting new session.")
 
 
-# generic greeting
+# skill is started - output: generic greeting
 def on_launch(launch_request, session):
     session_attributes = {}
     card_title = "RAQ"
@@ -59,12 +59,12 @@ def on_launch(launch_request, session):
     return build_response(session_attributes, card_title, speech_output, reprompt_text, should_end_session)
 
 
-#  session is over - message for our cloud formation stack
+#  session is over - output: message for our cloud formation stack
 def on_session_ended(session_ended_request, session):
     print("Ending session.")
 
 
-# handling all intents
+# handling all amazon-made intents, ends the session and closes the skill - output: generic farewell
 def handle_filler_request():
     session_attributes = {}
     card_title = "Thanks for visiting RAQ. Bye."
@@ -73,58 +73,81 @@ def handle_filler_request():
     return build_response(session_attributes, card_title, speech_output, None, should_end_session)
 
 
+# test intent - output: database version via alexa or generic cover-up message on connection error
 def get_test_value():
     session_attributes = {}
     card_title = "Test Value : DB Version"
-    reprompt_text = "Test Value : DB Version"
+    reprompt_text = "Something bad happened. Please try again later."
+    speech_output = "Something bad happened. Please try again later."
     should_end_session = False
-
     connection = connection_to_db()
+
     try:
         with connection.cursor() as cursor:
             cursor.execute("SELECT VERSION()")
             db_version = cursor.fetchone()
     except:
-        db_version = -1
+        db_version = "unknown"
     else:
         cursor.close()
     finally:
         connection.close()
+        speech_output = speech_output + str(db_version)
 
-    print(db_version)
-    speech_output = "Mr Johnson, our. database . well. how do i. say it. version is " + str(db_version)
     return build_response(session_attributes, card_title, speech_output, reprompt_text, should_end_session)
 
 
-def get_project_size_by_language():
+# "average size by language" intent - output: language, size in byte or generic cover-up message on connection error
+def get_project_size_by_language(intent):
     session_attributes = {}
-    card_title = "get project size by language"
-    reprompt_text = ""
-    should_end_session = False
-    speech_output = "Filler"
-    return build_response(session_attributes, card_title, speech_output, reprompt_text, should_end_session)
-
-
-def get_commits_with_curses_by_language():
-    session_attributes = {}
-    card_title = "hexes and curses"
-    speech_output = "hexes are bad. " \
-                    "really bad."
-    reprompt_text = "hexes are bad"
+    card_title = "Get the average project size for a language"
+    reprompt_text = "Sorry, I don't know that one. Please try again."
+    speech_output = "Sorry, I don't know that one. Please try again."
     should_end_session = False
 
+    if "programminglanguage" in intent["slots"]:
+        programminglanguage = intent["slots"]["programminglanguage"]["value"]
+
+        connection = connection_to_db()
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT VERSION()")
+                answer = cursor.fetchone()
+        except:
+            speech_output = "There was a connection error. Please try again later."
+            reprompt_text = "There was a connection error. Please try again later."
+        else:
+            reprompt_text = "Sorry, I don't know that one. Please try again."+ answer
+            speech_output = "Sorry, I don't know that one. Please try again."+ answer
+            cursor.close()
+        finally:
+            connection.close()
+
     return build_response(session_attributes, card_title, speech_output, reprompt_text, should_end_session)
 
 
-def get_languages_used_together():
+# "language with most commits with curses" intent - output: language, amount or generic cover-up message on connection error
+def get_commits_with_curses_by_language(intent):
     session_attributes = {}
-    card_title = "Filler"
-    reprompt_text = ""
+    card_title = "Get the average project size for a language"
+    reprompt_text = "Sorry, I don't know that one. Please try again."
+    speech_output = "Sorry, I don't know that one. Please try again."
     should_end_session = False
-    speech_output = "Filler"
+
     return build_response(session_attributes, card_title, speech_output, reprompt_text, should_end_session)
 
-# connect to Database - the encoded environment variables were put into the aws lambda console
+
+# "languages often used together" intent - output: language one and two or generic cover-up message on connection error
+def get_languages_used_together(intent):
+    session_attributes = {}
+    card_title = "Get the languages best buddy"
+    reprompt_text = "Sorry, I don't know that one. Please try again."
+    speech_output = "Sorry, I don't know that one. Please try again."
+    should_end_session = False
+    return build_response(session_attributes, card_title, speech_output, reprompt_text, should_end_session)
+
+
+# connection to database - the encoded environment variables were put into the aws lambda console
 def connection_to_db():
     return pymysql.connect(os.environ['DBENDPOINT'],
                            os.environ['DBUSER'],
@@ -133,7 +156,12 @@ def connection_to_db():
                            connect_timeout=5)
 
 
-# function used for building the answer (JSON)
+# function used for building the answer (JSON):
+# session_attributes    - won't be used in this application
+# title                 - card title for echo/simulator GUI
+# output                - speech text for the answer
+# reprompt_text         - speech text when nothing happened for a while
+# should_end_session    - ends the session/closes the skill when true
 def build_response(session_attributes, title, output, reprompt_text, should_end_session):
         return {
             "version": "1.0",
